55ff8322b11123d6523f0ee7a8c6ca349e6df06f
DESKTOP-G76K803\T <gh147862@gmail.com>
2023-09-07 03:00:54 +0800

    167. Two Sum II - Input Array Is Sorted

--- main.go
+++ main.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"fmt"
-	m "go-leet/ValidPalindrome"
+	m "go-leet/TwoSumIIInputArrayIsSorted"
 )
 
 func main() {
-	s := "0P"
-	res := m.P(s)
+	s := []int{2, 4, 5, 8}
+	target := 9
+	res := m.S(s, target)
 	fmt.Println(res)
 }
